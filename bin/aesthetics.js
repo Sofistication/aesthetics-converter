cc31250c4b60cf993ce04fb2421f7084935c2003
Sofistication <sordonez93@gmail.com>
2017-01-15 16:42:46 -0500

    Fix bin script
    -bin script now correctly runs
    -added some new outputs to make it clearer

--- bin/aesthetics.js
+++ bin/aesthetics.js
@@ -3,13 +3,9 @@
 const aes = require('../lib/aesthetics');
 const ask = require('../lib/ask');
 
-const aestheticizer = function () {
+console.log('Welcome to the land of A E S T H E T I C S !');
+let text = ask('What would you like to aestheticize?');
 
-  console.log('Welcome to the land of A E S T H E T I C S !');
-  let text = ask('What would you like to aestheticize?');
-
-  console.log('L E T \' S  G E T  A E S T H E T I C !');
-  aes(text);
-};
-
-// module.export = aestheticizer;
+console.log('L E T \' S  G E T  A E S T H E T I C !');
+console.log('     -----------     ');
+console.log(aes(text));
